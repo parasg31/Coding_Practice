dc58d3dbf56d021c312d3a393e248b4547f5000f
parasg31 <goelparas31@gmail.com>
2023-09-14 17:09:41 +0530

    Initial Commit

---
+++ Patterns/pattern4.cpp
@@ -0,0 +1,16 @@
+//Half pyramid problem
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the Number of your choice"<<endl;
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int col=0;col<=row;col++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
